74a371f08ebb2d8fcdc2fe8f366f79216f706884
Serizao <none@none.fr>
2022-05-09 20:16:19 +0200

    [FIX] optimisation mais y'as encore du boulot

--- frontend/src/app/admin/bugbountyStat/bugbountyStat.component.ts
+++ frontend/src/app/admin/bugbountyStat/bugbountyStat.component.ts
@@ -24,6 +24,7 @@ import { toBase64String } from '@angular/compiler/src/output/source_map';
 })
 export class BugbountyStatComponent implements OnInit {
 
+
   loadSyncInti=false
   loadSyncYwh=false
   scopeYWH = [
@@ -37,6 +38,7 @@ export class BugbountyStatComponent implements OnInit {
       "report_date":""
     }
   ]
+
   statYWH = {
     earnedEuro:0,
     collab_number:0,
@@ -54,7 +56,7 @@ export class BugbountyStatComponent implements OnInit {
     },
     earn_by_month:{
     }
-  }
+  };
   optionsYWHPie:any ;
   optionsYWHPieReportStatus: any;
   optionsYWHBarre: any = {};
@@ -97,6 +99,7 @@ statINTI = {
     }
   ]
 
+
   loading=true
 
   yeswehackExist=false
@@ -121,12 +124,12 @@ statINTI = {
         console.log(this.yeswehackExist)
         if(element.name == 'yeswehack'){
           this.yeswehackExist=true
-          this.getStatsYesWeHack()
+          this.getStatsPlatform('yeswehack')
           one=true
         } 
         if(element.name == 'intigriti'){
           this.intigritiExist = true
-          this.getStatsIntigriti()
+          this.getStatsPlatform('intigriti')
           one=true
         } 
       });     
@@ -138,12 +141,7 @@ statINTI = {
 
   }
 
-  getStatsYesWeHack(){
-    this.bugbountyPlatform.getScope('yeswehack').subscribe( (result) => {
-        this.scopeYWH=result.data
-        this.computeYWHStat()
-    })
-  }
+
 
 
   computeYWHStat() {
@@ -188,7 +186,7 @@ statINTI = {
     this.statYWH.average_per_rapport = parseFloat((this.statYWH.earnedEuro /this.scopeYWH.length).toFixed(2))
     this.loadingYWHGlobal=false
     console.log(this.statYWH)
-    this.initYWHPie()
+    this.optionsYWHPie = this.pieCriticity(this.statYWH.rapport_severity.L,this.statYWH.rapport_severity.M,this.statYWH.rapport_severity.H,this.statYWH.rapport_severity.C)
     this.initYWHBarre()
     this.initYWHPiReportStatus()
     this.initYWHEarnByMonth()
@@ -205,75 +203,25 @@ statINTI = {
     this.loadSyncYwh=true
     this.bugbountyPlatform.syncScope('yeswehack').subscribe( (result)=>{
       this.loadSyncYwh=false
-      this.getStatsYesWeHack()
+      this.getStatsPlatform('yeswehack')
     },(err) =>{
       this.loadSyncYwh=false
       this.showToast(err.message,'danger')
     })
   }
 
-  initYWHPie(){
-    this.optionsYWHPie = {
-      title: {
-        text: "Report by criticity",
-        //x: "center"
-      },
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-      },
-      calculable: true,
-      series: [
-        {
-          name: "Report by criticity",
-          type: "pie",
-          radius: [30, 110],
-          roseType: "area",
-          data: [
-            { value: this.statYWH.rapport_severity.L, name: "Low" },
-            { value: this.statYWH.rapport_severity.M, name: "Medium" },
-            { value: this.statYWH.rapport_severity.H, name: "High" },
-            { value: this.statYWH.rapport_severity.C, name: "Critical" }
-          ],
-          color:['#00d68f','#fa0','#ff3d71','#222b45']
-        }
-      ]
-    };
-  }
   initYWHPiReportStatus(){
     let data=Array()
     Object.keys(this.statYWH.report_by_status).forEach( (element) => {
       data.push({value:this.statYWH.report_by_status[element as keyof typeof this.statYWH.report_by_status],name:element})
     })
     console.log(data)
-    this.optionsYWHPieReportStatus = {
-      title: {
-        text: "Report by status",
-        //x: "center"
-      },
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-      },
-      calculable: true,
-      series: [
-        {
-          name: "Report by criticity",
-          type: "pie",
-          radius: [30, 110],
-          roseType: "area",
-          data: data,
-          color:['#00d68f','#fa0','#ff3d71','#222b45']
-        }
-      ]
-    };
+    this.optionsYWHPieReportStatus = this.pieRepport(data)
   }
 
   initYWHBarre(){
     let month=(new Date()).getMonth();
     let year=((new Date()).getFullYear()-2);
-    let tempM=0;
-    let tempY=0;
     let data=Array()
     let label=Array()
     for(var i=0;i<25;i++){
@@ -292,53 +240,12 @@ statINTI = {
       }
       month++
     }
-    this.optionsYWHBarre = {
-      title: {
-        text: "Report by month",
-        //x: "center"
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow',
-        },
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true,
-      },
-      xAxis: [
-        {
-          type: 'category',
-          data: label,
-          axisTick: {
-            alignWithLabel: true,
-          },
-        },
-      ],
-      yAxis: [
-        {
-          type: 'value',      
-        },
-      ],
-      series: [
-        {
-          name: 'Number of report',
-          type: 'bar',
-          barWidth: '60%',
-          data: data,
-        },
-      ],
-    };
+    this.optionsYWHBarre = this.graphBarre(data,label)
   }
   
   initYWHEarnByMonth(){
     let month=(new Date()).getMonth();
     let year=((new Date()).getFullYear()-2);
-    let tempM=0;
-    let tempY=0;
     let data=Array()
     let label=Array()
     for(var i=0;i<25;i++){
@@ -357,53 +264,21 @@ statINTI = {
       }
       month++
     }
-    this.optionsYWHEarnedByMonth = {
-      title: {
-        text: "Earn by month",
-        //x: "center"
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow',
-        },
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true,
-      },
-      xAxis: [
-        {
-          type: 'category',
-          data: label,
-          axisTick: {
-            alignWithLabel: true,
-          },
-        },
-      ],
-      yAxis: [
-        {
-          type: 'value',      
-        },
-      ],
-      series: [
-        {
-          name: 'Number of report',
-          type: 'bar',
-          barWidth: '60%',
-          data: data,
-        },
-      ],
-    };
+    this.optionsYWHEarnedByMonth = this.earnByMonth(data,label)
   }
   //---------------Intigriti part
 
-  getStatsIntigriti(){
-    this.bugbountyPlatform.getScope('intigriti').subscribe( (result) => {
+  getStatsPlatform(platform:string){
+    this.bugbountyPlatform.getScope(platform).subscribe( (result) => {
+      if(platform=='yeswehack'){
+        this.scopeYWH=result.data
+        this.computeYWHStat()
+      }
+      if(platform=='intigriti'){
         this.scopeINTI=result.data
         this.computeINTIStat()
+      }
+
     })
   }
 
@@ -437,9 +312,10 @@ statINTI = {
         //gain par mois
 
         //status des rapports
+        
         if(_.isUndefined(tempStatus[element.status as keyof typeof tempStatus]))tempStatus[element.status as keyof typeof tempStatus]=0
         tempStatus[element.status as keyof typeof tempStatus]++
-
+      })
         this.statINTI.report_by_month= temp
         this.statINTI.report_by_status = tempStatus
         this.statINTI.earn_by_month = tempEarnByMonth
@@ -447,18 +323,19 @@ statINTI = {
         this.statINTI.average_per_rapport = parseFloat((this.statINTI.earnedEuro /this.scopeINTI.length).toFixed(2))
         this.loadingINTIGlobal=false
         console.log(this.statINTI)
-        this.initINTIPie()
-        this.initINTIBarre()
-        this.initINTIPiReportStatus()
-        this.initINTIEarnByMonth()
-        console.log(this.statINTI)
-      })
+        this.optionsINTIPie =  this.pieCriticity(this.statINTI.rapport_severity.L,this.statINTI.rapport_severity.M,this.statINTI.rapport_severity.H,this.statINTI.rapport_severity.C)
+
+      
+      this.initINTIBarre()
+      this.initINTIPiReportStatus()
+      this.initINTIEarnByMonth()
+      console.log(this.statINTI)
   }
   syncINTI() {
     this.loadSyncInti=true
     this.bugbountyPlatform.syncScope('intigriti').subscribe( (result)=>{
       this.loadSyncInti=false
-      this.getStatsIntigriti()
+      this.getStatsPlatform('intigriti')
     },(err) =>{
       this.loadSyncInti=false
       this.loadingINTIGlobal = false;
@@ -466,68 +343,18 @@ statINTI = {
     })
   }
 
-  initINTIPie(){
-    this.optionsINTIPie = {
-      title: {
-        text: "Report by criticity",
-        //x: "center"
-      },
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-      },
-      calculable: true,
-      series: [
-        {
-          name: "Report by criticity",
-          type: "pie",
-          radius: [30, 110],
-          roseType: "area",
-          data: [
-            { value: this.statINTI.rapport_severity.L, name: "Low" },
-            { value: this.statINTI.rapport_severity.M, name: "Medium" },
-            { value: this.statINTI.rapport_severity.H, name: "High" },
-            { value: this.statINTI.rapport_severity.C, name: "Critical" }
-          ],
-          color:['#00d68f','#fa0','#ff3d71','#222b45']
-        }
-      ]
-    };
-  }
   initINTIPiReportStatus(){
     let data=Array()
     Object.keys(this.statINTI.report_by_status).forEach( (element) => {
       data.push({value:this.statINTI.report_by_status[element as keyof typeof this.statINTI.report_by_status],name:element})
     })
     console.log(data)
-    this.optionsINTIPieReportStatus = {
-      title: {
-        text: "Report by status",
-        //x: "center"
-      },
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-      },
-      calculable: true,
-      series: [
-        {
-          name: "Report by criticity",
-          type: "pie",
-          radius: [30, 110],
-          roseType: "area",
-          data: data,
-          color:['#00d68f','#fa0','#ff3d71','#222b45']
-        }
-      ]
-    };
+    this.optionsINTIPieReportStatus = this.pieRepport(data)
   }
 
   initINTIBarre(){
     let month=(new Date()).getMonth();
     let year=((new Date()).getFullYear()-2);
-    let tempM=0;
-    let tempY=0;
     let data=Array()
     let label=Array()
     for(var i=0;i<25;i++){
@@ -546,7 +373,76 @@ statINTI = {
       }
       month++
     }
-    this.optionsINTIBarre = {
+    this.optionsINTIBarre =  this.graphBarre(data,label)
+  }
+  
+  initINTIEarnByMonth(){
+    let month=(new Date()).getMonth();
+    let year=((new Date()).getFullYear()-2);
+    let data=Array()
+    let label=Array()
+    for(var i=0;i<25;i++){
+      let padMonth = `${month}`.padStart(2, '0')
+      let key:string = `${year}${padMonth}`
+      let tempLabel:string = `${year}-${padMonth}`
+      if(Object.prototype.hasOwnProperty.call(this.statINTI.earn_by_month,key)){
+       data.push(this.statINTI.earn_by_month[key as keyof typeof this.statINTI.earn_by_month])
+      } else {
+        data.push(0)
+      }
+      label.push(tempLabel)
+      if(month==12){
+        month=0
+        year++
+      }
+      month++
+    }
+    this.optionsINTIEarnedByMonth = this.earnByMonth(data,label)
+  }
+  earnByMonth(data:any[],label:any[]){
+    return{
+      title: {
+        text: "Earn by month",
+        //x: "center"
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: label,
+          axisTick: {
+            alignWithLabel: true,
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',      
+        },
+      ],
+      series: [
+        {
+          name: 'Number of report',
+          type: 'bar',
+          barWidth: '60%',
+          data: data,
+        },
+      ],
+    };
+  }
+  graphBarre(data:any[],label:any[]){
+    return {
       title: {
         text: "Report by month",
         //x: "center"
@@ -587,69 +483,53 @@ statINTI = {
       ],
     };
   }
-  
-  initINTIEarnByMonth(){
-    let month=(new Date()).getMonth();
-    let year=((new Date()).getFullYear()-2);
-    let tempM=0;
-    let tempY=0;
-    let data=Array()
-    let label=Array()
-    for(var i=0;i<25;i++){
-      let padMonth = `${month}`.padStart(2, '0')
-      let key:string = `${year}${padMonth}`
-      let tempLabel:string = `${year}-${padMonth}`
-      if(Object.prototype.hasOwnProperty.call(this.statINTI.earn_by_month,key)){
-       data.push(this.statINTI.earn_by_month[key as keyof typeof this.statINTI.earn_by_month])
-      } else {
-        data.push(0)
-      }
-      label.push(tempLabel)
-      if(month==12){
-        month=0
-        year++
-      }
-      month++
-    }
-    this.optionsINTIEarnedByMonth = {
+  pieRepport(data:any[]){
+    return {
       title: {
-        text: "Earn by month",
-        //x: "center"
+        text: "Report by status",
       },
       tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow',
-        },
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
       },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true,
-      },
-      xAxis: [
-        {
-          type: 'category',
-          data: label,
-          axisTick: {
-            alignWithLabel: true,
-          },
-        },
-      ],
-      yAxis: [
-        {
-          type: 'value',      
-        },
-      ],
+      calculable: true,
       series: [
         {
-          name: 'Number of report',
-          type: 'bar',
-          barWidth: '60%',
+          name: "Report by criticity",
+          type: "pie",
+          radius: [30, 110],
+          roseType: "area",
           data: data,
-        },
-      ],
+          color:['#00d68f','#fa0','#ff3d71','#222b45']
+        }
+      ]
+    };
+  }
+  pieCriticity(low:number,medium:number,high:number,critic:number){
+    return {
+      title: {
+        text: "Report by criticity",
+      },
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      calculable: true,
+      series: [
+        {
+          name: "Report by criticity",
+          type: "pie",
+          radius: [30, 110],
+          roseType: "area",
+          data: [
+            { value: low, name: "Low" },
+            { value: medium, name: "Medium" },
+            { value: high, name: "High" },
+            { value: critic, name: "Critical" }
+          ],
+          color:['#00d68f','#fa0','#ff3d71','#222b45']
+        }
+      ]
     };
   }
 }
