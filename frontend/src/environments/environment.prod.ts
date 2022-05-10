05b2b0144943945f58a5ea81b466a2f00f7d6c32
Serizao <none@none.fr>
2022-05-10 19:51:10 +0200

    [FEATURE] start angular as production mod with docker

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true
 };
 
-export const baseUrl = `http://127.0.0.1:3000`;
+export const baseUrl = ``;
 export const login = `/auth/login`;
 export const dashboard = `/dashboard`;
