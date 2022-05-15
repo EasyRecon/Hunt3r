4203f045dfb91b22e90822e708e6ba76a0ebf592
JoMar <contact@jomar.fr>
2022-05-15 14:35:54 +0200

    [FEATURE] Improve mesh sync by adding screenshots

--- backend/app/controllers/screenshots_controller.rb
+++ backend/app/controllers/screenshots_controller.rb
@@ -1,7 +1,7 @@
 class ScreenshotsController < ApplicationController
-  before_action :authenticate_user, except: %i[create_outside]
+  before_action :authenticate_user, except: %i[index_outside create_outside]
 
-  # GET /screenshots
+  # GET /screenshots/:subdomain_id
   def index
     screenshot = Screenshot.find_by(subdomain_id: params[:subdomain_id])
     if screenshot.nil?
@@ -11,6 +11,17 @@ class ScreenshotsController < ApplicationController
     decoded = Base64.decode64(screenshot.screenshot)
 
     send_data(decoded, type: 'image/png', disposition: 'inline')
+  end
+
+  # POST /screenshots/mesh
+  # Accessible from outside
+  def index_outside
+    mesh_infos = params.require(:meshs).permit(:url, :token, :subdomain)
+    return unless Mesh.find_by(url: mesh_infos[:url], token: mesh_infos[:token])
+
+    subdomain = Subdomain.find_by(url: mesh_infos[:subdomain])
+
+    render json: { message: nil, data: subdomain.screenshot[:screenshot] }, status: 200
   end
 
   # POST /screenshots
