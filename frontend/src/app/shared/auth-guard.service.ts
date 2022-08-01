a67e22f069b54352ea48f35f6cacb7747a65f56b
Serizao <none@none.fr>
2022-08-01 20:30:37 +0200

    [FIX] auth user redirect & noreferrer on subdomain link

--- frontend/src/app/shared/auth-guard.service.ts
+++ frontend/src/app/shared/auth-guard.service.ts
@@ -1,18 +1,37 @@
 import {Injectable} from '@angular/core';
 import {CanActivate, Router} from "@angular/router";
-import {NbAuthService} from "@nebular/auth";
+import {NbAuthService,NbAuthToken} from "@nebular/auth";
 import { NbToastrService, NbComponentStatus } from '@nebular/theme';
 import {tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthGuardService implements CanActivate {
+
+
+  user = {user_id:0,user_email:'',exp:0};
+
 
   constructor(private authService: NbAuthService,
               private router: Router,
               private toastrService: NbToastrService) {
                 //this.authService.isAuthenticated().subscribe( (x) => {console.log(x)})
+                this.authService.onTokenChange() 
+                .subscribe( (token: NbAuthToken) => {
+                if (token.isValid()) {
+                  this.user = token.getPayload(); // receive payload from token and assign it to our `user` variable
+                    console.log('check token')
+                    if(Math.floor(Date.now() / 1000) > this.user.exp){
+                      localStorage.clear();
+                      this.router.navigateByUrl('/auth/login');
+                    }
+                } else {
+                  localStorage.clear();
+                  this.router.navigateByUrl('/auth/login');
+                }
+              });
   }
 
   canActivate() {
