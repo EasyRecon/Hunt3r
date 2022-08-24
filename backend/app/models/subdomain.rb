6f6f427a5c3537609ca81b518f0658271bebb1af
JoMar <contact@jomar.fr>
2022-08-24 21:26:28 +0200

    [FEATURE] Sanitize input

--- backend/app/models/subdomain.rb
+++ backend/app/models/subdomain.rb
@@ -2,6 +2,6 @@ class Subdomain < ApplicationRecord
   belongs_to :domain
 
   scope :filtered_by_subdomain, ->(subdomain) { where('url LIKE ?', "%#{subdomain}%") }
-  scope :filtered_by_technology, ->(technology) { where('infos @> ?', "{\"technologies\": [{\"name\":\"#{technology}\"}]}") }
+  scope :filtered_by_technology, ->(technology) { where('infos @> ?', "{\"technologies\": [{\"name\":\"#{sanitize_sql_like(technology)}\"}]}") }
   scope :filtered_by_status_code, ->(status_code) { where('infos @> ?', "{\"status_code\": #{status_code}}") if status_code.to_i > 0 }
 end
